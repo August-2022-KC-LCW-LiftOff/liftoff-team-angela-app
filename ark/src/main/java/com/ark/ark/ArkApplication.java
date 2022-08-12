2550df56305ba950ad306b642971685d8b912688
Jamie Miller <dontlaughatme@yahoo.com>
2022-08-11 19:50:50 -0500

    initial commit

---
+++ ark/src/main/java/com/ark/ark/ArkApplication.java
@@ -0,0 +1,13 @@
+package com.ark.ark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkApplication.class, args);
+	}
+
+}
---
+++ ark/src/test/java/com/ark/ark/ArkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ark.ark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
