c420bf5850fd063c48053e4310a377d410bae03d
Jamie Miller <97189124+JustJamie313@users.noreply.github.com>
2022-08-29 12:38:38 -0500

    created request mapping feature  - updated ark.css to provide map stylings  - added small image for map markers  - updated AuthenticationController to process user location information  - updated createRequest.html to include request location information  - updated createRequestFormDTO to include location information  - updated editProfile.html to include location information  - updated EditProfileFormDTO to include location information  - updated fragments.html to include scripts from siteFragments.html  - added geocodeAddress.js to convert addresses to lat/lng locations  - updated index.html to use scripts from siteFragments.html  - added Location class to process user and request location information  - added MapController for url mapping  - added mapFragments.html containing the html fragments  - added maps.js to generate and customize the map  - updated register.html to include location information for user  - updated RegistrationFormDTO to include location information  - updated Request class to include request location information  - updated requestConfirmation.html to include siteFragments  - updated RequestController to process request location information  - updated requestFragments.html to add location fields to forms  - updated RequestRepository to create public requests query  - added showMap.html template  - updated siteFragments.html for map scripts  - updated User class to include location information  - updated userFragments.html to include location fields  - updated userRequests.html to use siteFragments.html scripts  - added USStates.java enum for US States list  - added viewRequest.html for testing purposes

---
+++ src/main/resources/static/js/geocodeAddress.js
@@ -0,0 +1,33 @@
+let geocoder;
+
+function getGeocodeFromAddress(){
+    geocoder = new google.maps.Geocoder();
+    let address = document.querySelector("#addressLine1").value;
+
+    if (document.querySelector("#addressLine2").value != ""){
+        address += ", " + document.querySelector("#addressLine2").value;
+    }
+    address += ", " + document.querySelector("#city").value;
+    address += ", " + document.querySelector("#state").value;
+    address += "  " + document.querySelector("#zipcode").value;
+        geocoder.geocode({'address':address},function(results, status){
+            if(status == 'OK'){
+                document.querySelector("#location").value = results[0].geometry.location.toString().replace("(","").replace(")","");
+                toggleSubmitAddressButton(false);
+            } else {
+                console.log('Geocode not successful: ' + status);
+            }
+        });
+        document.querySelector("#submitAddressButton").addEventListener('click',getGeocodeFromAddress);
+
+}
+function toggleSubmitAddressButton(show){
+    let btn = document.querySelector("#submitAddressButton");
+    if(show){
+        btn.classList.remove("hidden");
+        return;
+    }
+    btn.classList.add("hidden");
+}
+
+
