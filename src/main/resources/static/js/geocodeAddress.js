d339ab6d2ec4a4e4098ee12f6ebc265056a86ab5
Jamie Miller <dontlaughatme@yahoo.com>
2022-08-29 13:06:22 -0500

    updated geocodeAddress.js to correct empty field error on page load

--- src/main/resources/static/js/geocodeAddress.js
+++ src/main/resources/static/js/geocodeAddress.js
@@ -3,22 +3,24 @@ let geocoder;
 function getGeocodeFromAddress(){
     geocoder = new google.maps.Geocoder();
     let address = document.querySelector("#addressLine1").value;
-
-    if (document.querySelector("#addressLine2").value != ""){
-        address += ", " + document.querySelector("#addressLine2").value;
-    }
-    address += ", " + document.querySelector("#city").value;
-    address += ", " + document.querySelector("#state").value;
-    address += "  " + document.querySelector("#zipcode").value;
-        geocoder.geocode({'address':address},function(results, status){
-            if(status == 'OK'){
-                document.querySelector("#location").value = results[0].geometry.location.toString().replace("(","").replace(")","");
-                toggleSubmitAddressButton(false);
-            } else {
-                console.log('Geocode not successful: ' + status);
+    if(address != ""){
+        if (document.querySelector("#addressLine2").value != ""){
+                address += ", " + document.querySelector("#addressLine2").value;
             }
-        });
-        document.querySelector("#submitAddressButton").addEventListener('click',getGeocodeFromAddress);
+            address += ", " + document.querySelector("#city").value;
+            address += ", " + document.querySelector("#state").value;
+            address += "  " + document.querySelector("#zipcode").value;
+                geocoder.geocode({'address':address},function(results, status){
+                    if(status == 'OK'){
+                        document.querySelector("#location").value = results[0].geometry.location.toString().replace("(","").replace(")","");
+                        toggleSubmitAddressButton(false);
+                    } else {
+                        console.log('Geocode not successful: ' + status);
+                    }
+                });
+    }
+
+    document.querySelector("#submitAddressButton").addEventListener('click',getGeocodeFromAddress);
 
 }
 function toggleSubmitAddressButton(show){
