bfd5ae8bf53d9b1df572c0d2b258a12bd1a135e0
lindsaypettipiece <59128405+lindsaypettipiece@users.noreply.github.com>
2022-09-01 18:30:46 -0500

    updated requestController to include viewRequest, updated ResponseController, updated requestConfirmation to include Header and Footer

--- src/main/java/com/ark/demo/controllers/RequestController.java
+++ src/main/java/com/ark/demo/controllers/RequestController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -98,7 +95,8 @@ public class RequestController {
     }
 
     @GetMapping("viewRequest/{requestId}")
-    public String viewRequest(){
+    public String viewRequest(HttpServletRequest request, Model model, @PathVariable int requestId){
+        model.addAttribute("request", requestRepository.findById(requestId).get());
         return "requestTemplates/viewRequest";
     }
 
