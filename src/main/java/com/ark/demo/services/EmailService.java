2f9c1df507bc94fe026452d68f94b60f095dde54
Jamie Miller <dontlaughatme@yahoo.com>
2022-09-09 20:22:56 -0500

    added requestconfirmation template to email service

--- src/main/java/com/ark/demo/services/EmailService.java
+++ src/main/java/com/ark/demo/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.ark.demo.services;
 
+import com.ark.demo.models.Request;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -19,12 +20,8 @@ public class EmailService {
     public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
-    public void sendRegistrationEmail(String toEmail, String uid) throws MessagingException, UnsupportedEncodingException {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true);
-        helper.setFrom("ark-no-reply@domesne.com");
-        helper.setTo(toEmail);
-        helper.setSubject("Verify E-mail Address");
+    public void sendRegistrationEmail(String toEmail, String uid) {
+        String subject = "Verify E-mail Address";
         String text = String.format("""
                 <!DOCTYPE html>
                 <html lang="en">
@@ -81,7 +78,127 @@ public class EmailService {
                     </body>
                 </html>
                 """, uid);
-        helper.setText(text,true);
+        sendMail(toEmail,text, subject);
+    }
+    public void sendRequestConfirmation(String toEmail, Request request){
+        String subject = "New Request Created";
+        String text = String.format("""
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                    <style>
+                    .label {text-align:right;}
+                    .value {text-align:left;}
+                    tr:nth-child(odd){background:rgb(127,127,127);}
+                    </style>
+                    </head>
+                    <body style="text-align:center;">
+                    <div>
+                      <table>
+                        <tr>
+                          <td style="background:black;color:white;" colspan=3>
+                            ARK
+                          </td>
+                        <tr>
+                        <tr>
+                          <td colspan=3>
+                            <h1>A new request has been created!</h1>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td colspan=3>
+                            <h3>Request Details</h3>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">title:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">public:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">status:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">due date:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">address line 1:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">address line 2:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">city:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">state:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">zipcode:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="label">description:</td>
+                          <td colspan=2 class="value">
+                            %s
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                    </body>
+                </html>
+                """,
+                request.getTitle(),
+                request.getPublicEvent(),
+                request.getStatus(),
+                request.getDueDate(),
+                request.getAddressLine1(),
+                request.getAddressLine2(),
+                request.getCity(),
+                request.getState(),
+                request.getZipcode(),
+                request.getDescription());
+        sendMail(toEmail,text,subject);
+    }
+    private void sendMail(String toEmail, String text, String subject){
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try{
+            helper = new MimeMessageHelper(mimeMessage,true);
+            helper.setFrom("ark-no-reply@domesne.com");
+            helper.setTo(toEmail);
+            helper.setSubject("subject");
+            helper.setText(text,true);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         javaMailSender.send(mimeMessage);
     }
 }
